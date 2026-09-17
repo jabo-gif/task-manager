ab28290ee74fd7641e7c8f98bd1316b4496066d2
= <desiremjabo@gmail.com>
2026-09-17 08:37:05 +0200

    Add SSL support for managed MySQL

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -13,6 +13,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   dateStrings: true,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
 });
 
 module.exports = pool;
